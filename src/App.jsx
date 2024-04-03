b25a01cc31051c4f97eb6a89063d7b612e10bf8b
Bal Krishna Pandey <balkrishnapandey21@gmail.com>
2024-04-03 17:28:24 +0530

    REDUX

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css';
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos';
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Learn About Redux Toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit';
+
+import todoReducer from '../features/todo/todoSlice';
+
+
+
+
+export const store =configureStore({
+    reducer:todoReducer
+});
